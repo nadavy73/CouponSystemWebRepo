6544fc185deadaff1fa840fec75510e06e8e2549
yehiel <yehiel@LAPTOP-6H3UH84E>
2016-12-25 13:09:15 +0200

    Customer commit

--- WebContent/app/admin/adminController/adminCustomer.js
+++ WebContent/app/admin/adminController/adminCustomer.js
@@ -1,17 +1,54 @@
-admin.controller("customerCtrl", function($scope, $filter, $http) {
+admin.controller("customerCtrl",['$scope','CustomersService', function($scope,CustomersService) {
 	
-	$http.get("http://www.w3schools.com/angular/customers.php")
-    .then(function (response) {$scope.customers = response.data.records;});
+	$scope.sortType     = 'custName'; // set the default sort type
+	  $scope.sortReverse  = false;  // set the default sort order
+	  $scope.searchCustomer   = '';     // set the default search/filter term
+	
+	  $scope.CustArr = [];
+	  
+	  
+	  CustomersService.getCustomers().then(function (data){
+		  	$scope.customers = data.data;
+		  
+		  angular.element("#loader").hide();
+	  });
 
+$scope.nameValidation = function (name) {
+    if (name.length < 1) {
+        return "User name can't be empty";
+    } else {
+        return true;
+    }
+};   
+
+$scope.passwordValidation = function (password) {
+    var pattern = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{4,9}/g;
+    if (pattern.test(password) && (password.length < 10)) {
+        return true;
+    } else {
+        return "Password must contain:\n"
+            + "4-9 characters\n"
+            + "At lest one upper case letter\n"
+            + "At lest one lower case letter\n"
+            + "At lest one digit";
+    }
+
+};
+
+	
 	//Add new Customer
-	$scope.addNewCustomer = function(){
-		$scope.inserted = {
-		id: $scope.customers.length+1, 
-        name: customerss.name,
-        password: customers.password,
-				};
-	            $scope.customers.push($scope.inserted);
-	        };
+	$scope.addNew = function(customer){
+    	
+		  
+        $scope.customers.push({ 
+            'custName': $scope.fullname, 
+            'custPassword': $scope.password
+        });
+        
+        CustomersService.addCustomer({'custName' : $scope.fullname,custPassword : $scope.password})
+       //Todo:
+
+};
 	       
 	      //Remove Customer
 	        $scope.removeCustomer = function(index) {
@@ -71,4 +108,4 @@ admin.controller("customerCtrl", function($scope, $filter, $http) {
 	                personalDetails.selected = $scope.selectedAll;
 	            });
 	        };    
-	});
+	}]);
