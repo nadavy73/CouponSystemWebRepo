bfab71fbc73ed886d0ae1e395958f5c70946b73a
Ofer <Oferb1986@gmail.com>
2016-12-28 19:10:39 +0200

    Company changes

--- WebContent/app/admin/adminController/admin.company.service.js
+++ WebContent/app/admin/adminController/admin.company.service.js
@@ -10,26 +10,84 @@ admin.factory('CompaniesService',
         service.returnValue = {};
         service.returnValue.AllCompanies = {};
 
+        
         service.getCompanies = function (){
-     angular.element("#loader").show();   	
+        	angular.element("#loader").show();   	
         	return $http.get(url+"getallcompanies")
                .success(function (response) {
 
             });
         }
         
-        service.addCompany = function(fromClient) {
-      	  return $http.put(url+"createCompany",fromClient)
-      	  .sucess(function(data,status,headers,config)
-      			  {
-      		  console.log("data");
-      			  })
-      			  .error(function(data,status){
-      				  console.log("error:"+data)
-      			  })
-      		  
-      	  
-        }
         
-    return service;
+        
+        service.addCompany = function(company) {
+      	  return $http({
+              method: 'PUT',
+              url: url + "createCompany",
+              headers: {'Content-Type': 'application/json'},
+              data: company
+          })
+      };
+      	  
+      	  
+//		service.addCompany = function(fromClient) {
+//        	  return $http.put(url+"createCompany",fromClient)
+//        	  .success(function(data,status,headers,config)
+//        			  {
+//        		  console.log(data);
+//        			  })
+//        			  .error(function(data,status){
+//        				  console.log("error:"+data)
+//        			  })
+//        		  
+//        	  
+//          }
+        
+        service.removeCompany = function(ID) {
+        	return 	 $http({ 
+        		method : 'DELETE',
+        		url : url + "removeCompany",
+        		headers: {'Content-Type': 'text/plain'},
+        		data: ID
+        	});
+        };
+        	
+        	
+        //Update Company
+        service.updateCompany= function(id, company) {
+        	var companyToUpdate = angular.copy(company);
+            companyToUpdate.id = id;
+            
+        	return $http({
+                method: 'POST',
+                url: url + "updateCompany",
+                headers: {'Content-Type': 'application/json'},
+                data: companyToUpdate
+        	
+        	 });
+        };
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+//        	$http.post(url+"updateCompany", fromClient)
+// 
+//        	  .success(function(data,status,headers,config)
+//        		 {
+//        		  console.log(data);
+//        			  })
+//        			  .error(function(data,status){
+//        				  console.log("error:"+data)
+//        			  })
+//        }
+//        
+    
+        
+        
+        
+        return service;
 }]);
