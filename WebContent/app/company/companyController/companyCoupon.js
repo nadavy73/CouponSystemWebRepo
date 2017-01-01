1de283a2f8a6ac814e025707c5390019d8b666e2
Ofer <Oferb1986@gmail.com>
2017-01-01 19:48:46 +0200

    1/1/2017

--- WebContent/app/company/companyController/companyCoupon.js
+++ WebContent/app/company/companyController/companyCoupon.js
@@ -196,5 +196,23 @@ company.controller("companyCouponCtrl", ['$scope','$rootScope','companyCouponSer
                  }
              };   
    
+             
+             $scope.imageUpload = function(event){
+                 var files = event.target.files; //FileList object
+                 
+                 for (var i = 0; i < files.length; i++) {
+                     var file = files[i];
+                         var reader = new FileReader();
+                         reader.onload = $scope.imageIsLoaded; 
+                         reader.readAsDataURL(file);
+                 }
+            }
+
+            $scope.imageIsLoaded = function(e){
+                $scope.$apply(function() {
+                    $scope.coupons.push(e.target.result);
+                });
+            }      
+             
     
 }]);
--- WebContent/factory/couponFilter.factory.js
+++ WebContent/factory/couponFilter.factory.js
@@ -1,7 +1,6 @@
 // Used to created a new filter
 // the filter is used for sending filter coupon data to server
-app
-    .factory('couponFilterFactory', ['couponTypesFactory', function (couponTypesFactory) {
+app.factory('couponFilterFactory', ['couponTypesFactory', function (couponTypesFactory) {
         return function () {
             var couponTypes = angular.copy(couponTypesFactory);
             var couponFilter = {
